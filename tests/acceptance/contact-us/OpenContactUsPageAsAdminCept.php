791bda8969bc17e58353761fbbc2e609c92a1b72
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-08-02 13:34:17 +0300

    Updated test

--- tests/acceptance/contact-us/OpenContactUsPageAsAdminCept.php
+++ tests/acceptance/contact-us/OpenContactUsPageAsAdminCept.php
@@ -6,3 +6,7 @@ $I->wantTo('open contact us page as admin');
 $I->setHeader("Accept", "text/html");
 $I->amOnPage('/contact-us/');
 $I->see('Contact us');
+$I->dontSee('Name');
+$I->dontSee('Email');
+$I->see('Subject');
+$I->see('Message');
