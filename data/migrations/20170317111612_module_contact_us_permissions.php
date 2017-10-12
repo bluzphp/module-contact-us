b6f5e8cd37f49642113e4e3df8159555b0f6c66f
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-10-12 19:22:22 +0300

    Fixed migrations (divided into structure and data migrations)

---
+++ data/migrations/20170317111612_module_contact_us_permissions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright Bluz PHP Team
+ * @link https://github.com/bluzphp/skeleton
+ */
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * CreateContactUsTable
+ */
+class ModuleContactUsPermissions extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $data = [
+            [
+                'roleId' => 2,
+                'module' => 'contact-us',
+                'privilege' => 'Management'
+            ]
+        ];
+
+        $privileges = $this->table('acl_privileges');
+        $privileges->insert($data)
+            ->save();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->execute('DELETE FROM acl_privileges WHERE module = "contact-us"');
+    }
+}
